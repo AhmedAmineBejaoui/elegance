08cf695ecc2dfe0201ff1c47929e2c59d98bed3b
Ahmed Amin Bejaoui <bejaouiahmed053@gmail.com>
2025-08-15 21:34:32 +0100

    fix build

--- api/auth/user.ts
+++
@@ -1,4 +0,0 @@
-export default function handler(req: any, res: any) {
-  // Simule un utilisateur non connecté
-  res.status(200).json({ authenticated: false, user: null });
-}
--- api/categories.ts
+++
@@ -1,6 +0,0 @@
-export default function handler(req: any, res: any) {
-  res.status(200).json([
-    { id: 1, name: "Rings" },
-    { id: 2, name: "Necklaces" }
-  ]);
-}
